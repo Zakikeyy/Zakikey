584723a02fb1411ef6e2c0b2e631087b0dc5cbca
zakikeyy <zakidewasiwa95@gmail.com>
2023-12-21 13:39:41 +0700

    menambahkan portofolio

--- forms/contact.php
+++ forms/contact.php
@@ -7,14 +7,14 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'zakidewasiwa95@gmail.com';
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( '0e8a91e7d450eeb38e60ff4dc3956bde-us21');
+    die( 'Unable to load the "PHP Email Form" Library!');
   }
- 
+
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
